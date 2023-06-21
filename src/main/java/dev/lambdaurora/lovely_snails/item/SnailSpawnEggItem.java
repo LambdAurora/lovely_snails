00dea6ac6ca2f6370e9abd2bd77d5f5da24fc0e3
LambdAurora <email@lambdaurora.dev>
2023-06-21 17:57:38 +0200

    Fix dates.

--- src/main/java/dev/lambdaurora/lovely_snails/item/SnailSpawnEggItem.java
+++ src/main/java/dev/lambdaurora/lovely_snails/item/SnailSpawnEggItem.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022-2023 LambdAurora <email@lambdaurora.dev>
+ * Copyright (c) 2022 LambdAurora <email@lambdaurora.dev>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
