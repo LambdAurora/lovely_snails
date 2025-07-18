69bf2148df1aa89daabfd4bad3fde8d00732a75f
Patbox <39821509+Patbox@users.noreply.github.com>
2025-07-18 15:03:46 +0200

    Licenses have been applied

--- src/main/java/dev/lambdaurora/lovely_snails/client/render/SnailEntityRenderState.java
+++ src/main/java/dev/lambdaurora/lovely_snails/client/render/SnailEntityRenderState.java
@@ -1,9 +1,25 @@
+/*
+ * Copyright © 2025 LambdAurora <email@lambdaurora.dev>
+ *
+ * This file is part of Lovely Snails.
+ *
+ * Licensed under the Lambda License. For more information,
+ * see the LICENSE file.
+ */
+
 package dev.lambdaurora.lovely_snails.client.render;
 
 import net.minecraft.client.renderer.entity.state.LivingEntityRenderState;
 import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.ItemStack;
 
+/**
+ * Rendering data needed for snails
+ *
+ * @author Patbox
+ * @version 1.2.1
+ * @since 1.2.1
+ */
 public class SnailEntityRenderState extends LivingEntityRenderState {
     public boolean isScared = false;
     public DyeColor carpetColor = null;
--- src/main/java/dev/lambdaurora/lovely_snails/item/EquipmentContainer.java
+++ src/main/java/dev/lambdaurora/lovely_snails/item/EquipmentContainer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2022 LambdAurora <email@lambdaurora.dev>
+ * Copyright © 2025 LambdAurora <email@lambdaurora.dev>
  *
  * This file is part of Lovely Snails.
  *
